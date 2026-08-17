5778793b9a714a7bb8440e2200a363e63b1e84ca
princetq <princetq@gmail.com>
2026-08-17 18:13:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'phat-thuoc-pwa-v42-5-9-fix17-4-rt-single-zero-stuck-realtime1';
+const CACHE_NAME = 'phat-thuoc-pwa-v42-fix17-5-auto-collab-single-v1';
 
 const APP_SHELL = [
   '/',
