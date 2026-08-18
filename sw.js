d7dc983f2944d0606c675126666896d7b9223fb0
princetq <princetq@gmail.com>
2026-08-18 08:38:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'phat-thuoc-pwa-v42-fix17-5-5-auto-collab-keepalive-single-v1';
+const CACHE_NAME = 'phat-thuoc-pwa-v42-fix17-5-6-auto-collab-keepalive-single-v1';
 
 const APP_SHELL = [
   '/',
